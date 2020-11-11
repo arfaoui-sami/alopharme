a02283e062f8f98284b4d9c329561ffe78ef5613
sami.arfaoui@tek-up.tn <e7ye96XBA>
2020-11-11 03:19:17 +0100

    v11

---
+++ src/main/java/com/example/alopharm/AlopharmApplication.java
@@ -0,0 +1,13 @@
+package com.example.alopharm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlopharmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlopharmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/alopharm/AlopharmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alopharm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlopharmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
